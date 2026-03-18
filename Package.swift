d54500db30d01bb286a6694c63b76339cd636967
Asti Manuka <asti.manuka@gmail.com>
2026-03-18 16:26:13 +0100

    init

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.4
+
+import PackageDescription
+
+struct PackageMetadata {
+    static let version: String = "3.13.0"
+    static let checksum: String = "d7bbea4150746ce0452027835ce4443407d62ca1236c31ae60f0b3ccca8c4426"
+}
+
+let package = Package(
+    name: "spm-protobuf",
+    platforms: [
+        .iOS(.v10),
+        .tvOS(.v10),
+        .macOS(.v10_10)
+    ],
+    products: [
+        .library(
+            name: "Protobuf",
+            targets: ["Protobuf"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Protobuf",
+            url: "https://github.com/astmnk/spm-protobuf/releases/download/\(PackageMetadata.version)/Protobuf.xcframework.zip",
+            checksum: PackageMetadata.checksum)
+    ]
+)
